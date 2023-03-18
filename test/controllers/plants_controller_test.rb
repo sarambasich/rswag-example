96143e1d18416239832ce2e4151d9a0133bac92f
Stefan Arambasich <1455368+sarambasich@users.noreply.github.com>
2023-03-18 17:02:26 -0400

    Updates controller tests

--- test/controllers/plants_controller_test.rb
+++ test/controllers/plants_controller_test.rb
@@ -1,19 +1,36 @@
 require "test_helper"
 
+# These test cases use fixtures for pre-defined model instances
 class PlantsControllerTest < ActionDispatch::IntegrationTest
 	test "should get index" do
 		get plants_url
 		assert_response :success
+
+		data = JSON.parse(response.body)
+		assert(data.is_a?(Array))
+		assert_equal(2, data.count)
+
+		first_plant = data.first.symbolize_keys
+		assert(first_plant[:id].is_a?(Integer))
+		assert_equal("Stefan's Plant", first_plant[:name])
+		assert_equal("Peace Lily", first_plant[:species])
+		assert_has_timestamps(first_plant)
+
+		second_plant = data.last.symbolize_keys
+		assert(second_plant[:id].is_a?(Integer))
+		assert_equal("Other Plant", second_plant[:name])
+		assert_equal("Parlor Palm", second_plant[:species])
+		assert_has_timestamps(second_plant)
 	end
 
 	test "should get show" do
-		p = Plant.first! # 2 in fixtures
-		get plant_url(p)
+		plant = Plant.first!
+		get plant_url(plant)
 		assert_response :success
 
 		data = JSON.parse(response.body).deep_symbolize_keys
-		assert_equal(p.name, data[:name])
-		assert_equal(p.species, data[:species])
+		assert_equal(plant.name, data[:name])
+		assert_equal(plant.species, data[:species])
 		assert_has_timestamps(data)
 	end
 
@@ -33,8 +50,8 @@ class PlantsControllerTest < ActionDispatch::IntegrationTest
 	end
 
 	test "should PUT update" do
-		p = Plant.first!
-		put plant_url(p), params: {
+		plant = Plant.first!
+		put plant_url(plant), params: {
 			plant: {
 				name: 'Updated plant',
 				species: 'Dracaena'
@@ -46,6 +63,11 @@ class PlantsControllerTest < ActionDispatch::IntegrationTest
 		assert_equal('Updated plant', data[:name])
 		assert_equal('Dracaena', data[:species])
 		assert_has_timestamps(data)
+
+		plant.reload
+
+		assert_equal('Updated plant', plant.name)
+		assert_equal('Dracaena', plant.species)
 	end
 
 	test "should DELETE destroy" do
